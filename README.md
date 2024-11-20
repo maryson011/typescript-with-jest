cd720cce69442572d05e6bcdf22801cabe209c92
maryson011 <maryson011@gmail.com>
2024-11-20 17:02:19 -0300

    config: initial config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TypeScript with Jest
+
+ - npm init -y
+
+ - npm i typescript jest @types/jest ts-jest
+
+ - npx tsc --init
+    criar o tsconfig.json
---
+++ basicos/test/soma.test.ts
@@ -0,0 +1,6 @@
+import { soma } from "../src/soma"
+
+test("Deve testar a função soma", () => {
+    const resultado = soma(1,2)
+    expect(resultado).toBe(3)
+})
